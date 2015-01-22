133b796d753a4da4fcb19648c10b0b9998f43174
Robert Massaioli <rmassaioli@atlassian.com>
2015-01-23 09:04:21 +1100

    Using sets to actually work things out.
    A program about sets using sets. Now we are properly meta enough to be something that people will
    actually use. :P

--- SetInputVerification.hs
+++ SetInputVerification.hs
@@ -3,6 +3,7 @@ module SetInputVerification where
 import SetData
 import qualified Data.Text.Lazy as TL
 import Data.List
+import qualified Data.Set as S
 
 -- TODO these functions should generate potential lists of error messages
 
@@ -15,18 +16,19 @@ duplicateDefinitionName defs = fmap message problems
       problems = filter ((>1) . length) . group . sort . fmap definitionId $ defs
 
 unknownIdentifier :: Definitions -> [TL.Text]
-unknownIdentifier defs = fmap message nonDefinedIds
+unknownIdentifier defs = fmap message (S.toList nonDefinedIds)
    where
       message :: Identifier -> TL.Text
       message problem = TL.pack "Attempted to use an identifier that has not been defined: " `TL.append` problem
 
-      nonDefinedIds = usedIds \\ definitionIds
-      usedIds = concatMap extractIdsFromExpression . fmap definitionExpression $ defs
-      definitionIds = fmap definitionId defs
+      nonDefinedIds = usedIds S.\\ definitionIds
+      usedIds = extractIdsFromDefinitions defs
+      definitionIds = S.fromList . fmap definitionId $ defs
 
-extractIdsFromExpression :: Expression -> [Identifier]
-extractIdsFromExpression (BinaryExpression _ a b) = extractIdsFromExpression a ++ extractIdsFromExpression b
-extractIdsFromExpression (FileExpression _) = []
-extractIdsFromExpression (IdentifierExpression i) = [i]
+extractIdsFromExpression :: Expression -> S.Set Identifier
+extractIdsFromExpression (BinaryExpression _ a b) = extractIdsFromExpression a `S.union` extractIdsFromExpression b
+extractIdsFromExpression (FileExpression _) = S.empty
+extractIdsFromExpression (IdentifierExpression i) = S.singleton i
 
-
+extractIdsFromDefinitions :: Definitions -> S.Set Identifier
+extractIdsFromDefinitions = foldr S.union S.empty . fmap (extractIdsFromExpression . definitionExpression)
